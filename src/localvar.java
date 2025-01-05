90bd5bb7d8a817668b5403d5f43e68bae04145a2
vasanthakumar2708 <vasanthakumarg.27@gmail.com>
2025-01-05 21:40:17 +0530

    init commit

---
+++ src/localvar.java
@@ -0,0 +1,26 @@
+public class localvar {
+    public static void main(String[] args) {
+        int x;
+        //System.out.println(x);//for local variable jvm does not provide default value,
+                              // we have to intialize it.
+    }
+}
+
+class localdemo{
+    public static void main(String[] args) {
+        int n=4;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                i=i+j;
+            }
+        }
+       // System.out.println(i+"....."+j);
+    }
+}
+
+/*
+here scope of the local variable is within the methods or within the blocks or within the loops,
+so the scope of the local variable is same as the blocks.
+Local variables will be created in the execution of the blocks.
+Local variables are stored in stack area.
+ */
