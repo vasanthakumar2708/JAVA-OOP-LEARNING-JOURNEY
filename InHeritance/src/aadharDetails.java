b2258c203f8b600f2f81fb8f440cd0727b0e4f46
vasanthakumar2708 <vasanthakumarg.27@gmail.com>
2025-01-10 22:16:47 +0530

    init commit

---
+++ InHeritance/src/aadharDetails.java
@@ -0,0 +1,49 @@
+abstract class Person{
+    String name;
+    String aadhar;
+
+    Person(String name, String aadhar){
+        this.name=name;
+        this.aadhar=aadhar;
+    }
+}
+class Student extends Person{
+    int sid;
+
+    Student(String name, String aadhar, int sid){
+        super(name, aadhar);
+        this.sid=sid;
+    }
+}
+
+class Employee extends Person{
+    int eid;
+
+    Employee(String name, String aadhar, int eid){
+        super(name, aadhar);
+        this.eid=eid;
+    }
+}
+public class aadharDetails {
+    public static void main(String[] args) {
+        Student s = new Student("vasanth","94542536",1);
+        Employee e = new Employee("manoj","948464433",4);
+        //employee
+        System.out.println(e.name);
+        System.out.println(e.aadhar);
+        System.out.println(e.eid);
+        //student
+        System.out.println(s.name);
+        System.out.println(s.aadhar);
+        System.out.println(s.sid);
+    }
+}
+
+/*
+here i created a person class which contains common details and i created classes like student and employee
+i added a specific fiels in the child classes.
+
+To access parent class i used super keyword
+
+This is an example of hierarchial inheritance.
+ */
