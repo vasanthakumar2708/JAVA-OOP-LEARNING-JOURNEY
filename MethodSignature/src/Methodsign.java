839407df4d30bd01aace0f0755f20196b12303f0
vasanthakumar2708 <vasanthakumarg.27@gmail.com>
2025-01-18 13:12:51 +0530

    init commit

---
+++ MethodSignature/src/Methodsign.java
@@ -0,0 +1,39 @@
+/*
+what is method signature?
+In java method signature is consist of method name followed by the argument type but not argument names is called
+method signature.
+
+modifiers and return type are not part of the method signature.
+
+In any java class two methods with same method signature is not allowed.
+
+Java compiler uses method signature to resolve method invocation
+ */
+
+
+class Methodsign {
+    public void m1(int a, float b){   //valid
+
+    }
+    public int m1(int x, int c){
+        return x;
+
+    }
+}
+class demo{
+    public static void main(String[] args) {
+        Methodsign m = new Methodsign();
+
+
+    }
+}
+
+/*
+public void m1(int a, float b){    here it will throw compilation error since both method
+                                   signatures are same
+
+    }
+    public int m1(int x, float y){
+
+    }
+ */
